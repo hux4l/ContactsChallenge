06d80e3d661c0269a8df43bb8adba8cc4f34c813
tobas <tomas.baska@outlook.com>
2022-01-28 14:20:02 +0100

    working adding new contact

--- src/main/java/sk/tobas/contactschallenge/ContactsController.java
+++ src/main/java/sk/tobas/contactschallenge/ContactsController.java
@@ -6,11 +6,21 @@ import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.TableView;
+import javafx.scene.layout.BorderPane;
 import sk.tobas.contactschallenge.model.Contact;
 import sk.tobas.contactschallenge.model.ContactData;
 
+import java.io.IOException;
+import java.util.Optional;
+
 public class ContactsController {
+
+    @FXML
+    private BorderPane bpMain;
 
     @FXML
     private TableView<Contact> twContacts;
@@ -21,8 +31,34 @@ public class ContactsController {
         new Thread(task).start();
     }
 
+    // show dialog when add contact is pressed
     @FXML
-    public void handleAdd(ActionEvent actionEvent) {
+    public void handleAdd() {
+        Dialog<ButtonType> dialog = new Dialog<>();
+
+        dialog.initOwner(bpMain.getScene().getWindow());
+        dialog.setTitle("Add new Contact");
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("addContact.fxml"));
+
+        try {
+            dialog.getDialogPane().setContent(fxmlLoader.load());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == ButtonType.OK) {
+
+            AddContactController controller = fxmlLoader.getController();
+            Contact contact = controller.createContact();
+            twContacts.getSelectionModel().select(contact);
+        }
+
     }
 
     @FXML
